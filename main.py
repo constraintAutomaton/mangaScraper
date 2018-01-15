015b7e873afca96bbffafc3ed14a079be38048f3
bryan.elliott.tam <tambryanelliott@gmail.com>
2018-01-15 00:42:57 -0500

    reformate whit autopep8

--- main.py
+++ main.py
@@ -7,99 +7,102 @@ from interface import Ui_MainWindow
 import threading
 import queue
 
-global  downloadQueue
+global downloadQueue
 global mangaScraper
 
 mangaScraper = mangaFinder()
-downloadQueue = queue.Queue() # a queue to limit one download at the time 
+downloadQueue = queue.Queue()  # a queue to limit one download at the time
+
 
 class downloadingThread(QtCore.QThread):
     newOperation = QtCore.pyqtSignal()
     taskProgressBar = QtCore.pyqtSignal(tuple)
+
     def __init__(self):
-        
+
         QtCore.QThread.__init__(self)
         self.completed = 0
-        self.pageDowloaded = 0        
+        self.pageDowloaded = 0
+
     def run(self):
-        
-        while not downloadQueue.empty(): # start to download everything that is in the queue
-            
+
+        while not downloadQueue.empty():  # start to download everything that is in the queue
+
             infoDownload = downloadQueue.get()
-            mangaScraper.setVariable(infoDownload[0],infoDownload[1],infoDownload[2],infoDownload[3])
-            
+            mangaScraper.setVariable(
+                infoDownload[0], infoDownload[1], infoDownload[2], infoDownload[3])
+
             for pageDowloaded in mangaScraper.mangafoxDowload():
                 try:
-                     
-                    self.completed = (mangaScraper.pageDownloaded/mangaScraper.totalPage)*100
+
+                    self.completed = (
+                        mangaScraper.pageDownloaded / mangaScraper.totalPage) * 100
                     self.pageDowloaded = mangaScraper.pageDownloaded
                 except:
                     self.completed = 0
-                     
+
                 completed_dowloaded = (self.completed, self.pageDowloaded)
                 self.taskProgressBar.emit(completed_dowloaded)
         self.newOperation.emit()
-            
+
+
 class interface(Ui_MainWindow):
-    
+
     def __init__(self, w):
         self.setupUi(w)
         self.downloadingThread = None
-        
-        with open('lastUsed.txt','r') as file:
-            lastUsed = file.readlines()     
-        
+
+        with open('lastUsed.txt', 'r') as file:
+            lastUsed = file.readlines()
+
         self.leFolder.setText(lastUsed[0][0:-1])
         self.leUrl.setText(lastUsed[1][0:-1])
         self.leStart.setText(lastUsed[2][0:-1])
         self.leEnd.setText(lastUsed[3])
-            
-        
+
         self.btnGo.clicked.connect(self.btnDownload)
         self.btnAddQueue.clicked.connect(self.addToQueue)
-        
-        
+
     def addToQueue(self):
-        
-        if self.leUrl.text() == '' or self.leStart.text() == '' or self.leEnd.text() == '' or self.leFolder.text() =='':
+
+        if self.leUrl.text() == '' or self.leStart.text() == '' or self.leEnd.text() == '' or self.leFolder.text() == '':
             pass
 
         else:
-            
-            tupleInfoDownload = (self.leUrl.text(),float(self.leStart.text()),float(self.leEnd.text()),self.leFolder.text()) # data to download the chapter
-            downloadQueue.put(tupleInfoDownload) 
-            
-            self.tbQueueDownload.setText(self.tbQueueDownload.toPlainText()+ ' {} from chapter {} to chapter {} to {}\n'.format(mangaScraper.mangafoxGetTitle(tupleInfoDownload[0]), 
-                                                                                                                                tupleInfoDownload[1],tupleInfoDownload[2],tupleInfoDownload[3])) 
-            
+
+            tupleInfoDownload = (self.leUrl.text(), float(self.leStart.text()), float(
+                self.leEnd.text()), self.leFolder.text())  # data to download the chapter
+            downloadQueue.put(tupleInfoDownload)
+
+            self.tbQueueDownload.setText(self.tbQueueDownload.toPlainText() + ' {} from chapter {} to chapter {} to {}\n'.format(mangaScraper.mangafoxGetTitle(tupleInfoDownload[0]),
+                                                                                                                                 tupleInfoDownload[1], tupleInfoDownload[2], tupleInfoDownload[3]))
+
         # write on the label the queue
     def btnDownload(self):
-        
+
         self.addToQueue()
-        
+
         self.downloadingThread = downloadingThread()
         self.downloadingThread.start()
-        
-        
+
         self.downloadingThread.newOperation.connect(self.newOperation)
         self.downloadingThread.taskProgressBar.connect(self.taskProgressBar)
-        
+
         self.btnAddQueue.setEnabled(False)
-        self.btnGo.setEnabled(False)        
-                
-        
-    def taskProgressBar(self,completed_dowloaded):
+        self.btnGo.setEnabled(False)
+
+    def taskProgressBar(self, completed_dowloaded):
         try:
             self.lblProgressBarResult.setText('{} downloaded out of {}'.format(completed_dowloaded[1],
                                                                                mangaScraper.totalPage))
         except:
             self.lblProgressBarResult.setText('0 downloaded out of 0')
         self.progressBar.setValue(completed_dowloaded[0])
-        
+
     def newOperation(self):
         self.btnAddQueue.setEnabled(True)
         self.btnGo.setEnabled(True)
-        
+
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
@@ -108,5 +111,6 @@ def main():
     w.show()
     sys.exit(app.exec_())
 
+
 if __name__ == '__main__':
-    main() 
+    main()
--- mangaScraping.py
+++ mangaScraping.py
@@ -1,4 +1,4 @@
-import sys 
+import sys
 import bs4 as bs
 import requests as request
 import time
@@ -6,69 +6,71 @@ import os
 
 
 class mangaFinder():
-    def __init__(self,url='',start=0.0,end=0.0,folder=os.getcwd()):
+    def __init__(self, url='', start=0.0, end=0.0, folder=os.getcwd()):
         try:
             self.url = url
             self.chStart = start
-            self.chEnd = end   
+            self.chEnd = end
             self.domain = self.url[:self.url.find('.')]
             self.folder = folder
             self.totalPage = 0
-            self.pageDownloaded = 0  
-            
+            self.pageDownloaded = 0
+
             # Error handling
-            
-            if not(isinstance(self.url,str)):
+
+            if not (isinstance(self.url, str)):
                 raise TypeError('The url have to be a string')
-            
-            elif not(isinstance(self.chEnd,float)) and not(isinstance(self.chStart,float)):
-                
-                raise TypeError('The start and ending chapter have to be number')
-            
-            elif not(os.path.isdir(r'{}'.format(folder))):
+
+            elif not (isinstance(self.chEnd, float)) and not (isinstance(
+                    self.chStart, float)):
+
+                raise TypeError(
+                    'The start and ending chapter have to be number')
+
+            elif not (os.path.isdir(r'{}'.format(folder))):
                 raise ValueError("The directory don't exist")
-                               
-            
-            elif self.chStart>self.chEnd:
-                
-                raise ValueError('The ending chapter have to be after the start chapter')
-        
-        except TypeError as error: # maybe find a way to catch the error in one line
-    
+
+            elif self.chStart > self.chEnd:
+
+                raise ValueError(
+                    'The ending chapter have to be after the start chapter')
+
+        except TypeError as error:  # maybe find a way to catch the error in one line
+
             print(repr(error))
-            
+
         except ValueError as error:
-            
+
             print(repr(error))
-            
-    
-        
-            
+
     def mangafoxDowload(self):
         try:
             if self.domain == 'http://mangafox':
                 pass
             else:
-                raise ValueError('Url not valid for the moment we only support manga fox ')
+                raise ValueError(
+                    'Url not valid for the moment we only support manga fox ')
         except ValueError as error:
-            
+
             print(repr(error))
-        
+
         self.totalPage = 0
         self.pageDownloaded = 0
-        
-        allChapt = self.mangafoxGetChapter() # get all the chapters ask by the user
+
+        allChapt = self.mangafoxGetChapter(
+        )  # get all the chapters ask by the user
         self.mangafoxGetTotalPage(allChapt)
-        
-        for urlCh in allChapt: 
+
+        for urlCh in allChapt:
             try:
                 folder = self.mangafoxNameFolder(urlCh)
-                os.makedirs(r'{}\{}'.format(self.folder,folder))
+                os.makedirs(r'{}\{}'.format(self.folder, folder))
             except:
                 pass
-            
-            allPage = self.mangafoxGetAllPageChapter(urlCh) # get all the page of the user
-            
+
+            allPage = self.mangafoxGetAllPageChapter(
+                urlCh)  # get all the page of the user
+
             for urlPage in allPage:
                 sauce = ''
                 while sauce == '':
@@ -76,65 +78,64 @@ class mangaFinder():
                         sauce = request.get(urlPage, verify=False)
                     except:
                         time.sleep(5)
-                        continue                
-                 
+                        continue
+
                 soup = bs.BeautifulSoup(sauce.text, 'html.parser')
                 #  initiation of Bs object
-                
-                end = urlPage.find('html')-1
-                start = urlPage.rfind('/')+1
+
+                end = urlPage.find('html') - 1
+                start = urlPage.rfind('/') + 1
                 fileName = urlPage[start:end]
-                # name of the jpg file    
-                
-                result = soup.find('img')        
+                # name of the jpg file
+
+                result = soup.find('img')
                 stringResult = str(result)
-                
-                #find the tag whit the link of the image 
+
+                # find the tag whit the link of the image
                 start = stringResult.find('src=')
                 urlImage = stringResult[start:]
-                end = urlImage.find(' ')-1
+                end = urlImage.find(' ') - 1
                 urlImage = urlImage[5:end]
-                urlImage = urlImage.replace('amp;','')
+                urlImage = urlImage.replace('amp;', '')
                 # get the link of the image by delete the 'junk text' of the tag
-                
-                if not(os.path.isfile(r"{}\{}\{}.jpg".format(self.folder,folder,fileName))):
+
+                if not (os.path.isfile(r"{}\{}\{}.jpg".format(
+                        self.folder, folder, fileName))):
                     img_data = request.get(urlImage).content
                     with open('{}.jpg'.format(fileName), 'wb') as handler:
-                        handler.write(img_data) 
-                    self.manageImage(fileName,folder)
+                        handler.write(img_data)
+                    self.manageImage(fileName, folder)
                 else:
                     pass
-                
+
                 self.pageDownloaded += 1
                 yield self.pageDownloaded
-               
-                #dowload the image 
-           
-            
-                
+
+                # dowload the image
+
     def mangafoxGetChapter(self):
-        #initiation of bs
+        # initiation of bs
         sauce = ''
         while sauce == '':
             try:
                 sauce = request.get(self.url, verify=False)
             except:
                 time.sleep(5)
-                
-                continue        
-            
+
+                continue
+
         soup = bs.BeautifulSoup(sauce.text, 'html.parser')
         allResult = soup.find_all('a')
-        
-        resultatChapter = [] # list of chapter
-        
-        temp = [] # list of tag whitout the chapter
+
+        resultatChapter = []  # list of chapter
+
+        temp = []  # list of tag whitout the chapter
         # get tag whit chapter
         for result in allResult:
-            testAgent = self.url[5:] # false
+            testAgent = self.url[5:]  # false
             testAgent2 = 'title'
-            
-            if  testAgent in str(result) and testAgent2 in str(result):
+
+            if testAgent in str(result) and testAgent2 in str(result):
                 pass
             else:
                 temp.append(result)
@@ -143,24 +144,26 @@ class mangaFinder():
             allResult.remove(el)
         # extract the link out of the tag
         for result in allResult:
-            start = str(result).find('href="') +6
+            start = str(result).find('href="') + 6
             end = str(result).find('" title')
-            link = 'http:'+str(result)[start:end]
+            link = 'http:' + str(result)[start:end]
             # get the chapter input
-            start = link.rfind('c')+1
+            start = link.rfind('c') + 1
             end = link.rfind('/')
             numberchapt = link[start:end]
             for x in range(0, 2):
                 if numberchapt[0] == '0':
                     numberchapt = numberchapt[1:]
-            if float(numberchapt)<=self.chEnd and float(numberchapt)>=self.chStart:
-                resultatChapter.append(link)     
-        
+            if float(numberchapt) <= self.chEnd and float(
+                    numberchapt) >= self.chStart:
+                resultatChapter.append(link)
+
         resultatChapter.reverse()
         return resultatChapter
-    def mangafoxGetAllPageChapter(self,chapterUrl):
-        
-        #initiation of bs
+
+    def mangafoxGetAllPageChapter(self, chapterUrl):
+
+        # initiation of bs
         sauce = ''
         while sauce == '':
             try:
@@ -171,77 +174,81 @@ class mangaFinder():
                 continue
         soup = bs.BeautifulSoup(sauce.text, 'html.parser')
         allResult = soup.find_all('a')
-        
-        #find select tag who contains the text of the page of the chapter
-        allResult = soup.find_all('select') 
-        
+
+        # find select tag who contains the text of the page of the chapter
+        allResult = soup.find_all('select')
+
         tagNumberPage = str(allResult[1])
-        
-        start = find_nth(tagNumberPage,'value="',2,'r')+7
-        end = tagNumberPage.find('"',start)
+
+        start = find_nth(tagNumberPage, 'value="', 2, 'r') + 7
+        end = tagNumberPage.find('"', start)
         numberPage = int(tagNumberPage[start:end])
-        listNumberPage =[]
-        
-        while  numberPage>0:
+        listNumberPage = []
+
+        while numberPage > 0:
             listNumberPage.append(numberPage)
-            numberPage-=1
+            numberPage -= 1
         listNumberPage.reverse()
         listPageUrl = []
-        
+
         for page in listNumberPage:
-            end = chapterUrl.rfind('/')+1
-            urlPage = chapterUrl[0:end]+str(page)+'.html'
+            end = chapterUrl.rfind('/') + 1
+            urlPage = chapterUrl[0:end] + str(page) + '.html'
             listPageUrl.append(urlPage)
-       
+
         return listPageUrl
-    def mangafoxNameFolder(self,urlCh):
-        start = find_nth(urlCh,'/',4,'r')+1
+
+    def mangafoxNameFolder(self, urlCh):
+        start = find_nth(urlCh, '/', 4, 'r') + 1
         name = urlCh[start:]
-        loopName = name # can't loop and modify
-        
+        loopName = name  # can't loop and modify
+
         for caracter in loopName:
-            
-            if caracter=='_' or caracter=='/' :
-    
-                name = name.replace(caracter,' ')
-        name = name.replace(' 1.html','')
+
+            if caracter == '_' or caracter == '/':
+
+                name = name.replace(caracter, ' ')
+        name = name.replace(' 1.html', '')
         return name
-    def mangafoxGetTotalPage(self,allChapter):
-        
-         # get the number of pages of all the chapter the user wants to download
+
+    def mangafoxGetTotalPage(self, allChapter):
+
+        # get the number of pages of all the chapter the user wants to download
         for chapter in allChapter:
             self.totalPage += len(self.mangafoxGetAllPageChapter(chapter))
-    def mangafoxGetTitle(self,url):
-        start = find_nth(url,'/',4)+1
-        end = len(url)
-        Title = url[start:end-1]
-        return Title
-        
-        
-    def manageImage(self,image,folder):
-        
-        os.rename(r'{}.jpg'.format(image), r"{}\{}\{}.jpg".format(self.folder,folder,image))
-    def setVariable(self,url,start,end,folder):
-        self.__init__(url,start,end,folder)
 
-    
-def find_nth(string,substring,nb,direction = 'f'):
-    # find the position of the nth occurence of a substring 
-    
+    def mangafoxGetTitle(self, url):
+        start = find_nth(url, '/', 4) + 1
+        end = len(url)
+        Title = url[start:end - 1]
+        return Title
+
+    def manageImage(self, image, folder):
+
+        os.rename(r'{}.jpg'.format(image), r"{}\{}\{}.jpg".format(
+            self.folder, folder, image))
+
+    def setVariable(self, url, start, end, folder):
+        self.__init__(url, start, end, folder)
+
+
+def find_nth(string, substring, nb, direction='f'):
+    # find the position of the nth occurence of a substring
+
     beg = string.find(substring)
     if direction == 'f':
-        while nb>1 and beg<len(string):
-            beg = string.find(substring,beg+1)
+        while nb > 1 and beg < len(string):
+            beg = string.find(substring, beg + 1)
             nb -= 1
     else:
         past = ''
         result = [beg]
         while True:
-            
-            beg = string.find(substring,beg+1)
+
+            beg = string.find(substring, beg + 1)
             result.append(beg)
-            if result[len(result)-1] ==-1:
+            if result[len(result) - 1] == -1:
                 break
-        beg = result[len(result)-nb-1]
+        beg = result[len(result) - nb - 1]
     result = beg
     return result
