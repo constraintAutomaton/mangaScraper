99b5d273929cb5886ce3dd9b4c66105f950ebd85
bryan.elliott.tam <tambryanelliott@gmail.com>
2018-01-26 22:35:11 -0500

    find_nth test done

--- tests/test_mangaScrapper.py
+++ tests/test_mangaScrapper.py
@@ -13,27 +13,17 @@ class mangaScrapping_test(unittest.TestCase):
         self.mangaScrapper_2  = mangaFinder(url='http://mangafox.la/manga/the_gamer/',start =5,end=6)
     def test_find_nth(self):
         string = 'I test my function test1, test2, test3'
-        testFoward = find_nth(string,'test',1)
-        testFoward_2 = find_nth(string,'test',2)
-        testFoward_100 = find_nth(string,'test',100)
+     
+        self.assertEqual(find_nth(string,'test',1),2)
+        self.assertEqual(find_nth(string,'test',2),19)
+        self.assertRaises(ValueError,lambda: find_nth(string,'test',100))
         
-        testBackward = find_nth(string,'test',1,direction='b')
-        testBackward_2 = find_nth(string,'test',2,direction='b')
-        testBackward_100 = find_nth(string,'test',100,direction='b')        
+        self.assertEqual(find_nth(string,'test',1,direction='b'),33)
+        self.assertEqual(find_nth(string,'test',2,direction='b'),26)
+        self.assertRaises(ValueError,lambda: find_nth(string,'test',100,direction='b'))
         
-        testFowardMissingForward = find_nth(string,'missing',1)
-        testFowardMissingBackward = find_nth(string,'missing',1,direction='b')
-        
-        self.assertEqual(testFoward,2)
-        self.assertEqual(testFoward_2,19)
-        self.assertEqual(testFoward_100,33)
-        
-        self.assertEqual(testBackward,33)
-        self.assertEqual(testBackward_2,26)
-        self.assertEqual(testBackward_100,2)
-        
-        self.assertEqual(testFowardMissingForward,'')
-        self.assertEqual(testFowardMissingBackward,'')
+        self.assertRaises(ValueError,lambda: find_nth(string,'missing',1))
+        self.assertRaises(ValueError,lambda: find_nth(string,'missing',1,direction='b'))
         
 if __name__ == '__main__':
     unittest.main()
