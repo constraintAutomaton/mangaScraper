fe1c0470ce35b72cd0838bd1233e9c0c0fd1ecfc
bryan.elliott.tam <tambryanelliott@gmail.com>
2018-04-11 10:10:19 -0400

    still woriking in the pdf class

--- mangaScrapper/jpgToPdf.py
+++ mangaScrapper/jpgToPdf.py
@@ -1,13 +1,16 @@
 from fpdf import FPDF
+import cv2
+import os
 
 
 class PDF(FPDF):
-    def __init__(self,imageList=[]):
+    def __init__(self,chapterName, imageList=[]):
         FPDF.__init__(self)
         self.imageList=imageList
         self.sizePdfPortraitX = 210
         self.sizePdfPaysageX = 240
         self.imageCount = 0
+        self.chapterName = chapterName+'.pdf' 
     def chapter_body(self):
         if self.imageCount<len(self.imageList):
             self.image(self.imageList[self.imageCount],0,0,self.sizePdfPortraitX)
@@ -18,4 +21,9 @@ class PDF(FPDF):
         self.chapter_body()
     def Change_image_list(self,imageList):
         self.imageList = imageList
-
+    def run(self):
+        if not os.path.exists('pdf'):        
+            os.makedirs('pdf')        
+        for  image in self.imageList:
+            self.print_chapter()
+        self.output(os.path.join('pdf',self.chapterName),'F')
